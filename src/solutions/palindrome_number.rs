c227979c06b73e8345bcc4f90699866acbfc7945
rodrigocitadin <citadinrodrigo@gmail.com>
2025-06-29 20:19:38 -0300

    palindrome seems good

---
+++ src/solutions/palindrome_number.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn is_palindrome(x: i32) -> bool {
+        let chars: Vec<char> = x.to_string().chars().collect();
+        let len = chars.len();
+        let mid = len / 2;
+
+        for i in 0..mid {
+            let j = len - 1 - i;
+            if chars[i] != chars[j] {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}

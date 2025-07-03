73a802aa0c1b271213329f1d80dfef360dca22c5
rodrigocitadin <citadinrodrigo@gmail.com>
2025-07-02 21:09:11 -0300

    another version

--- src/solutions/longest_common_prefix.rs
+++ src/solutions/longest_common_prefix.rs
@@ -1,13 +1,10 @@
 pub fn longest_common_prefix(strs: Vec<String>) -> String {
-    let first = strs[0].chars().collect::<Vec<char>>();
+    let mut chars = strs[0].chars();
     let mut prefix = String::new();
 
-    for i in 0..first.len() {
-        let c = first[i];
-        for s in &strs {
-            if s.chars().nth(i) != Some(c) {
-                return prefix;
-            }
+    for (i, c) in chars.enumerate() {
+        if strs.iter().any(|s| s.chars().nth(i) != Some(c)) {
+            break;
         }
 
         prefix.push(c);

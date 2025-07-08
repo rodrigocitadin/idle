5f39cabb9e4ad6b18f9e23b8b322ca78d40df6da
rodrigocitadin <citadinrodrigo@gmail.com>
2025-07-08 14:46:53 -0300

    solve solution impl problems

--- src/solutions/merge_two_sorted_lists.rs
+++ src/solutions/merge_two_sorted_lists.rs
@@ -1,35 +1,35 @@
-// Definition for singly-linked list.
-#[derive(PartialEq, Eq, Clone, Debug)]
-pub struct ListNode {
-    pub val: i32,
-    pub next: Option<Box<ListNode>>,
-}
+// // Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//     pub val: i32,
+//     pub next: Option<Box<ListNode>>,
+// }
 
-impl ListNode {
-    #[inline]
-    fn new(val: i32) -> Self {
-        ListNode { next: None, val }
-    }
-}
+// impl ListNode {
+//     #[inline]
+//     fn new(val: i32) -> Self {
+//         ListNode { next: None, val }
+//     }
+// }
 
-pub fn merge_two_lists(
-    list1: Option<Box<ListNode>>,
-    list2: Option<Box<ListNode>>,
-) -> Option<Box<ListNode>> {
-    match (list1, list2) {
-        (Some(mut l1), Some(mut l2)) => {
-            if l1.val < l2.val {
-                let next = Self::merge_two_lists(l1.next.take(), Some(l2));
-                l1.next = next;
-                Some(l1)
-            } else {
-                let next = Self::merge_two_lists(Some(l1), l2.next.take());
-                l2.next = next;
-                Some(l2)
-            }
-        }
-        (Some(l1), None) => Some(l1),
-        (None, Some(l2)) => Some(l2),
-        (None, None) => None,
-    }
-}
+// pub fn merge_two_lists(
+//     list1: Option<Box<ListNode>>,
+//     list2: Option<Box<ListNode>>,
+// ) -> Option<Box<ListNode>> {
+//     match (list1, list2) {
+//         (Some(mut l1), Some(mut l2)) => {
+//             if l1.val < l2.val {
+//                 let next = Self::merge_two_lists(l1.next.take(), Some(l2));
+//                 l1.next = next;
+//                 Some(l1)
+//             } else {
+//                 let next = Self::merge_two_lists(Some(l1), l2.next.take());
+//                 l2.next = next;
+//                 Some(l2)
+//             }
+//         }
+//         (Some(l1), None) => Some(l1),
+//         (None, Some(l2)) => Some(l2),
+//         (None, None) => None,
+//     }
+// }
--- src/solutions/roman_to_integer.rs
+++ src/solutions/roman_to_integer.rs
@@ -1,28 +1,26 @@
 use std::collections::HashMap;
 
-impl Solution {
-    pub fn roman_to_int(s: String) -> i32 {
-        fn value(c: char) -> i32 {
-            match c {
-                'I' => 1,
-                'V' => 5,
-                'X' => 10,
-                'L' => 50,
-                'C' => 100,
-                'D' => 500,
-                'M' => 1000,
-                _ => 0,
-            }
+pub fn roman_to_int(s: String) -> i32 {
+    fn value(c: char) -> i32 {
+        match c {
+            'I' => 1,
+            'V' => 5,
+            'X' => 10,
+            'L' => 50,
+            'C' => 100,
+            'D' => 500,
+            'M' => 1000,
+            _ => 0,
         }
-
-        let numbers: Vec<i32> = s.chars().map(value).collect();
-
-        numbers.iter().enumerate().fold(0, |acc, (i, &num)| {
-            if i + 1 < numbers.len() && num < numbers[i + 1] {
-                acc - num
-            } else {
-                acc + num
-            }
-        })
     }
+
+    let numbers: Vec<i32> = s.chars().map(value).collect();
+
+    numbers.iter().enumerate().fold(0, |acc, (i, &num)| {
+        if i + 1 < numbers.len() && num < numbers[i + 1] {
+            acc - num
+        } else {
+            acc + num
+        }
+    })
 }

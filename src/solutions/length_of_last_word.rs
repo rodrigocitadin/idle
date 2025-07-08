c3cd11306ccbfd2284147cb4fcf9643572345e7b
rodrigocitadin <citadinrodrigo@gmail.com>
2025-07-08 14:56:21 -0300

    leaving both solutions

--- src/solutions/length_of_last_word.rs
+++ src/solutions/length_of_last_word.rs
@@ -5,3 +5,8 @@ pub fn length_of_last_word(s: String) -> i32 {
         .map(|word| word.len() as i32)
         .unwrap_or(0)
 }
+
+pub fn length_of_last_word_two(s: String) -> i32 {
+    let result: Vec<&str> = s.trim().split(" ").collect();
+    result[result.len() - 1].len() as i32
+}

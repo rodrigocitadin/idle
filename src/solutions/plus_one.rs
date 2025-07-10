3a08c8db7807eca30a43ac2d4eff135a745893a3
rodrigocitadin <citadinrodrigo@gmail.com>
2025-07-10 09:16:56 -0300

    morning

---
+++ src/solutions/plus_one.rs
@@ -0,0 +1,13 @@
+pub fn plus_one(mut digits: Vec<i32>) -> Vec<i32> {
+    for i in (0..digits.len()).rev() {
+        if digits[i] < 9 {
+            digits[i] += 1;
+            return digits;
+        }
+        digits[i] = 0;
+    }
+
+    let mut result = vec![0; digits.len() + 1];
+    result[0] = 1;
+    result
+}
